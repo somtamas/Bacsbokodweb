59acf7651f43995900faa1ec46b27980af748141
somlyaitamas <somlyaitamas@turr.hu>
2026-01-23 13:24:07 +0100

    SCRUM-35 dropdown fejlécben jelentős előrehaladás

--- main.js
+++ main.js
@@ -1,12 +1,38 @@
-function onkormanyzat() {
-    document.getElementById("myDropdownon").classList.toggle("show");
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+function myFunction2() {
+  document.getElementById("myDropdown2").classList.toggle("show");
+}
+
+function myFunction3() {
+  document.getElementById("myDropdown3").classList.toggle("show");
 }
 
 window.onclick = function(e) {
-    if (!e.target.matches('.dropbtnon')) {
-    let myDropdownon = document.getElementById("myDropdownon");
-      if (myDropdownon.classList.contains('show')) {
-        myDropdownon.classList.remove('show');
-      }
+  if (!e.target.matches('.dropbtn')) {
+  var myDropdown = document.getElementById("myDropdown");
+    if (myDropdown.classList.contains('show')) {
+      myDropdown.classList.remove('show');
     }
+  }
+}
+
+window.onclick = function(e) {
+  if (!e.target.matches('.dropbtn2')) {
+  var myDropdown2 = document.getElementById("myDropdown2");
+    if (myDropdown2.classList.contains('show')) {
+      myDropdown2.classList.remove('show');
+    }
+  }
+}
+
+window.onclick = function(e) {
+  if (!e.target.matches('.dropbtn3')) {
+  var myDropdown2 = document.getElementById("myDropdown3");
+    if (myDropdown2.classList.contains('show')) {
+      myDropdown2.classList.remove('show');
+    }
+  }
 }
