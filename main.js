93246508a25758907890d3f9345f26e85466918f
somlyaitamas <somlyaitamas@turr.hu>
2026-01-29 14:13:39 +0100

    SCRUM-35 meg kell oldani, hogy szépen jelenjenek meg a dropdown menüpontok, és azt próbáltam megoldani.

--- main.js
+++ main.js
@@ -12,27 +12,33 @@ function myFunction3() {
 
 window.onclick = function(e) {
   if (!e.target.matches('.dropbtn')) {
-  var myDropdown = document.getElementById("myDropdown");
+    myDropdown2.classList.remove('show2');
+    myDropdown3.classList.remove('show3');
+    var myDropdown = document.getElementById("myDropdown");
     if (myDropdown.classList.contains('show')) {
-      myDropdown.classList.remove('show');
+      myDropdown2.classList.remove('show');
     }
   }
 }
 
 window.onclick = function(f) {
   if (!f.target.matches('.dropbtn2')) {
-  var myDropdown2 = document.getElementById("myDropdown2");
+    myDropdown2.classList.remove('show');
+    myDropdown3.classList.remove('show3');
+    var myDropdown2 = document.getElementById("myDropdown2");
     if (myDropdown2.classList.contains('show2')) {
-      myDropdown2.classList.remove('show2');
+      myDropdown.classList.remove('show2');
     }
   }
 }
 
 window.onclick = function(g) {
   if (!g.target.matches('.dropbtn3')) {
-  var myDropdown3 = document.getElementById("myDropdown3");
+    myDropdown2.classList.remove('show2');
+    myDropdown3.classList.remove('show');
+    var myDropdown3 = document.getElementById("myDropdown3");
     if (myDropdown3.classList.contains('show3')) {
-      myDropdown3.classList.remove('show3');
+      myDropdown2.classList.remove('show3');
     }
   }
 }
