9414ba13455362082489224ef28f361ecd5552ec
somlyaitamas <somlyaitamas@turr.hu>
2026-02-03 08:45:18 +0100

    SCRUM-35 navbar próbálkozás

--- main.js
+++ main.js
@@ -1,44 +1,40 @@
+var myDropdown = document.getElementById("myDropdown");
+var myDropdown2 = document.getElementById("myDropdown2");
+var myDropdown3 = document.getElementById("myDropdown3");
 function myFunction() {
-  document.getElementById("myDropdown").classList.toggle("show");
+  myDropdown.classList.toggle("show");
 }
 
 function myFunction2() {
-  document.getElementById("myDropdown2").classList.toggle("show2");
+  myDropdown2.classList.toggle("show2");
 }
 
 function myFunction3() {
-  document.getElementById("myDropdown3").classList.toggle("show3");
+  myDropdown3.classList.toggle("show3");
 }
 
 window.onclick = function(e) {
   if (!e.target.matches('.dropbtn')) {
     myDropdown2.classList.remove('show2');
     myDropdown3.classList.remove('show3');
-    var myDropdown = document.getElementById("myDropdown");
     if (myDropdown.classList.contains('show')) {
-      myDropdown2.classList.remove('show');
+      myDropdown.classList.remove('show');
     }
   }
-}
 
-window.onclick = function(f) {
-  if (!f.target.matches('.dropbtn2')) {
-    myDropdown2.classList.remove('show');
+  if(!e.target.matches('.dropbtn2')) {
+    myDropdown.classList.remove('show');
     myDropdown3.classList.remove('show3');
-    var myDropdown2 = document.getElementById("myDropdown2");
     if (myDropdown2.classList.contains('show2')) {
-      myDropdown.classList.remove('show2');
+      myDropdown2.classList.remove('show2');
     }
   }
-}
 
-window.onclick = function(g) {
-  if (!g.target.matches('.dropbtn3')) {
+  if(!e.target.matches('.dropbtn3')) {
     myDropdown2.classList.remove('show2');
-    myDropdown3.classList.remove('show');
-    var myDropdown3 = document.getElementById("myDropdown3");
+    myDropdown.classList.remove('show');
     if (myDropdown3.classList.contains('show3')) {
-      myDropdown2.classList.remove('show3');
+      myDropdown3.classList.remove('show3');
     }
   }
 }
